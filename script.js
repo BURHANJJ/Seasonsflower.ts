3acd320960cd8373f31c08104b60cf570de65f3e
BURHANJJ <burhanjewjoubb@gmail.com>
2025-02-19 23:16:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const images = [
-    'https://media.discordapp.net/attachments/1341116521581379666/1341116558654832733/SmallWhiteFlower.gif?ex=67b4d3a0&is=67b38220&hm=69c01739b67baee28b09c3edba05663706a967e3692b98423b30918d91b146d5&=&width=160&height=160',
-    'https://media.discordapp.net/attachments/1341116521581379666/1341116559304953896/white_flower.png?ex=67b4d3a0&is=67b38220&hm=93a01907875691edee4524714f9a837dff9abfe75996551e19c0fe53977ff39b&=&format=webp&quality=lossless&width=547&height=547',
-    'https://media.discordapp.net/attachments/1341116521581379666/1341117194888679434/blue_heart.png?ex=67b4d438&is=67b382b8&hm=0e4618b0daf29b1970907aaa328f06b97b33b95a2186863d7ec40defb090d96e&=&format=webp&quality=lossless&width=547&height=547',
-    'https://media.discordapp.net/attachments/1341116521581379666/1341117195178213422/love.gif?ex=67b4d438&is=67b382b8&hm=adbf6cbabeaf3f960d1c8dcca7abb39f209d0e7289e73f29abf630da15d440d6&=&width=120&height=120',
+    'https://i.postimg.cc/G2fP2Gcp/Small-White-Flower.gif',
+    'https://i.postimg.cc/T2ZSNWrG/white-flower.png',
+    'https://i.postimg.cc/Y24ZxxLb/blue-heart.png',
+    'https://i.postimg.cc/ThMrjx5M/love.gif',
 ];
 
 const container = document.body;
